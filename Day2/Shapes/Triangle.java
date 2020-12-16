50f78ed65acce9a4389fe997d5488de38ea3a048
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-12-16 15:26:43 -0600

    triangle display currently borks when base>height

--- Day2/Shapes/Triangle.java
+++ Day2/Shapes/Triangle.java
@@ -24,14 +24,23 @@ public class Triangle implements Shape {
     public void display() {
         int largeHeight = (int) (height * 10);
         int largeBase = (int) (base * 10);
-        int slope = -((largeHeight) / (largeBase));
+        int slope = (largeHeight) / (largeBase);
+        int b = 0;
+        if (slope == 0) {
+            slope = largeBase / largeHeight;
+            b = (int) base;
+        }
         System.out.println(slope);
+        int d;
         for (int y = 0; y < largeHeight; y++) {
-            for (int x = 0; x < largeBase; x++) {
-                if (x == 0 || y == 0) {
+            try {
+                d = largeBase - ((y - b) / slope);
+            } catch (ArithmeticException e) {
+                d = 1;
+            }
+            for (int x = 0; x < d; x++) {
+                if (x == 0 || y == 0 || x == d - 1) {
                     System.out.print("*");// these are reversed,
-                } else if (y == slope * x + largeHeight) {
-                    System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
@@ -42,7 +51,7 @@ public class Triangle implements Shape {
     }
 
     public static void main(String[] args) {
-        Triangle tri = new Triangle(5, 5);
+        Triangle tri = new Triangle(7, 6);
         double area = tri.calculateArea();
         System.out.println(area);
         tri.display();
