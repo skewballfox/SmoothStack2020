011e1b3d41ba14411772c014c256a88888fc98a7
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-12-16 16:03:33 -0600

    working on making the circle display more circular

--- Day2/Shapes/Circle.java
+++ Day2/Shapes/Circle.java
@@ -2,9 +2,9 @@ package Day2.Shapes;
 
 public class Circle implements Shape {
 
-    double radius;
+    int radius;
 
-    public Circle(double r) {
+    public Circle(int r) {
         radius = r;
     }
 
@@ -16,7 +16,32 @@ public class Circle implements Shape {
 
     @Override
     public void display() {
+        int gridSize = radius * 2;
+        int center = radius;
+        if (radius % 2 == 0) {
+            // in order to have a center
+            gridSize += 1;
+            center += 1;
+        }
 
+        for (int y = 0; y < gridSize; y++) {
+            for (int x = 0; x < gridSize; x++) {
+                // System.out.println((int) Math.sqrt(Math.pow(x - center, 2) + Math.pow(y -
+                // center, 2)));
+                if (radius == (int) Math.sqrt(Math.pow(x - center, 2) + Math.pow(y - center, 2))) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
     }
 
+    public static void main(String[] args) {
+        Circle circ = new Circle(15);
+        System.out.println(circ.calculateArea());
+        circ.display();
+    }
 }
