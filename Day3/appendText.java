20c7833dd7350ad5ed0a12b4a7e4db903902dded
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-12-17 13:06:24 -0600

    finished appendText

--- Day3/appendText.java
+++ Day3/appendText.java
@@ -1,5 +1,27 @@
 package Day3;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class appendText {
 
+    public static void main(String[] args) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(args[0], true))) {
+            writer.newLine();
+            writer.write(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+            writer.newLine();
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("there was an error opening the file");
+            e.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("you forgot to supply an argument");
+            System.exit(0);
+        }
+
+    }
 }
